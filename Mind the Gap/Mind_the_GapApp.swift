3bee7f8fddc50941af6d84bf5c97bb6f1478a228
jahnavi patel <jvip715@gmail.com>
2023-06-12 21:23:58 -0500

    Initial Commit

---
+++ Mind the Gap/Mind_the_GapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mind_the_GapApp.swift
+//  Mind the Gap
+//
+//  Created by Jahnavi Patel on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Mind_the_GapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
