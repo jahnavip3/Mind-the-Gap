5f6cac1930864b2ac425bad808a13ab55736c45e
jahnavi patel <jvip715@gmail.com>
2023-06-13 21:02:36 -0500

    color

--- Mind the Gap/ContentView.swift
+++ Mind the Gap/ContentView.swift
@@ -13,19 +13,30 @@ struct ContentView: View {
         NavigationStack {
             VStack {
                 Text("Mind the Gap")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+    
                 NavigationLink(destination: AddressIssueView()) {
                     Text("Address the Issue")
+                        .foregroundColor(Color.black)
                         .multilineTextAlignment(.center)
+                    
+                
+                        
                 }
                 .padding()
                 
                 NavigationLink(destination: ComparisonsView()) {
-                        Text("Comparisons")
+                    Text("Comparisons")
+                        .foregroundColor(Color.black)
+                    
                         .multilineTextAlignment(.center)
+                        
                 }
                 .padding()
                 NavigationLink(destination: AwarenessHelpView()) {
                     Text("Awareness and Ways to Help")
+                        .foregroundColor(Color.black)
                         .multilineTextAlignment(.center)
                 }
                 .padding()
